2c1936f264d55c59f7fc504580d1d8b647409d38
timotheejonquay <timothee.jonquay@telecom-sudparis.eu>
2025-11-22 19:41:53 +0100

    Tout le monde est bourré

--- Assets/Passants/Passants.cs
+++ Assets/Passants/Passants.cs
@@ -5,9 +5,11 @@ public class Passants : MonoBehaviour
     [SerializeField] private Rigidbody rb;
     [SerializeField] private float speed = 1;
     [SerializeField] public Vector3 direction;
-    [SerializeField] public int type = 1;
+    [SerializeField] public int type;
     [SerializeField] int damage = 1;
     [SerializeField] float changeDirInterval = 2f;
+    [SerializeField] float[] probaType = {0.9f, 0.1f};
+    [SerializeField] Material materialBourre;
     float timer = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -15,7 +17,25 @@ public class Passants : MonoBehaviour
     {
         direction = new Vector3(Random.Range(-1f, 1f),
                                 0f,
-                                Random.Range(-1f, 1f)).normalized;;
+                                Random.Range(-1f, 1f)).normalized;
+        float alea=Random.Range(0f,1f);
+        print(alea);
+        float cumul = 0f;
+        for(int i = 0; i < probaType.Length; i++)
+        {
+            cumul+=probaType[i];
+            print(cumul);
+            if (alea <= cumul)
+            {
+                type = i+1;
+                
+            }
+            
+        }
+        if (type == 2)
+        {
+            GetComponent<Renderer>().material = materialBourre;
+        }
     }
 
     // Update is called once per frame
