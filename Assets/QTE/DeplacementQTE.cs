656f6f766f1129d9b6be448f5e92c71ba9be6ff0
SwiloPlt <corentin.pichon.92@gmail.com>
2025-11-22 14:18:19 +0100

    QTE défilement fini

--- Assets/QTE/DeplacementQTE.cs
+++ Assets/QTE/DeplacementQTE.cs
@@ -3,13 +3,13 @@ using UnityEngine.UI;
 
 public class DeplacementQTE : MonoBehaviour
 {
-    [SerializeField] private float speed = 100f;
+    [SerializeField] private float speed = 20f;
 
     private void Update()
     {
         transform.position += speed * Time.deltaTime * new Vector3(0, -1, 0);
 
-        if (transform.position.y <= -700)
+        if (transform.position.y <= -100)
         {
             QTEManager.Instance.SpawnTouch();
             Destroy(gameObject);
