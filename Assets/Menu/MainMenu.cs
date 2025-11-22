7fba29c275cef37318150bea7f82691d6028a41a
Ryane Seri <sryane046@gmail.com>
2025-11-22 16:57:10 +0100

    Add Button_Functions

---
+++ Assets/Menu/MainMenu.cs
@@ -0,0 +1,96 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [Header("Références UI")]
+    public Button btnDance;
+    public Button btnCommands;
+    public Button btnSettings;
+    public Button btnClose;
+    public GameObject windowsCommand;
+    public GameObject windowsSettings;
+    public GameObject buttonPanel; // Le panel qui contient tous les boutons principaux
+
+    [Header("Game Management")]
+    public string gameSceneName = "GameScene";
+
+    void Start()
+    {
+        // Cache les fenêtres au début
+        if (windowsCommand != null)
+            windowsCommand.SetActive(false);
+        if (windowsSettings != null)
+            windowsSettings.SetActive(false);
+        if (btnClose != null)
+            btnClose.gameObject.SetActive(false);
+
+        // Configure les événements des boutons
+        btnDance.onClick.AddListener(StartGame);
+        btnCommands.onClick.AddListener(ShowCommandsWindow);
+        btnSettings.onClick.AddListener(ShowSettingsWindow);
+        btnClose.onClick.AddListener(HideAllWindows);
+    }
+
+    public void StartGame()
+    {
+        Debug.Log("Lancement du jeu !");
+        // SceneManager.LoadScene(gameSceneName);
+    }
+
+    public void ShowCommandsWindow()
+    {
+        // Cache les boutons principaux
+        if (buttonPanel != null)
+            buttonPanel.SetActive(false);
+
+        // Affiche la fenêtre commands et le bouton close
+        if (windowsCommand != null)
+            windowsCommand.SetActive(true);
+        if (btnClose != null)
+            btnClose.gameObject.SetActive(true);
+
+        // S'assure que l'autre fenêtre est fermée
+        if (windowsSettings != null)
+            windowsSettings.SetActive(false);
+    }
+
+    public void ShowSettingsWindow()
+    {
+        // Cache les boutons principaux
+        if (buttonPanel != null)
+            buttonPanel.SetActive(false);
+
+        // Affiche la fenêtre settings et le bouton close
+        if (windowsSettings != null)
+            windowsSettings.SetActive(true);
+        if (btnClose != null)
+            btnClose.gameObject.SetActive(true);
+
+        // S'assure que l'autre fenêtre est fermée
+        if (windowsCommand != null)
+            windowsCommand.SetActive(false);
+    }
+
+    public void HideAllWindows()
+    {
+        // Cache toutes les fenêtres et le bouton close
+        if (windowsCommand != null)
+            windowsCommand.SetActive(false);
+        if (windowsSettings != null)
+            windowsSettings.SetActive(false);
+        if (btnClose != null)
+            btnClose.gameObject.SetActive(false);
+
+        // Réaffiche les boutons principaux
+        if (buttonPanel != null)
+            buttonPanel.SetActive(true);
+    }
+
+    // Méthode utilitaire pour vérifier l'état
+    public bool IsAnyWindowOpen()
+    {
+        return (windowsCommand != null && windowsCommand.activeSelf) ||
+               (windowsSettings != null && windowsSettings.activeSelf);
+    }
+}
