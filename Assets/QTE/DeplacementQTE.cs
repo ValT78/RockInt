64ac25a9cf06a6e0ffd86425cec65503cfaf69f4
SwiloPlt <corentin.pichon.92@gmail.com>
2025-11-22 12:01:12 +0100

    Début du QTE

---
+++ Assets/QTE/DeplacementQTE.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeplacementQTE : MonoBehaviour
+{
+    [SerializeField] private float speed = 100f;
+
+    private void Update()
+    {
+        transform.position += speed * Time.deltaTime * new Vector3(0, -1, 0);
+
+        if (transform.position.y <= -100)
+        {
+            QTEManager.Instance.SpawnTouch();
+            Destroy(gameObject);
+        }
+    }
+}
